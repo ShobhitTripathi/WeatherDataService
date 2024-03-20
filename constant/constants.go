a3bb6b156a9df9f952138856a6fd17a22ae610ee
Shobhit Tripathi <61062688+ShobhitTripathi@users.noreply.github.com>
2024-03-21 00:01:21 +0530

    Create constants.go

---
+++ constant/constants.go
@@ -0,0 +1,19 @@
+package constant
+
+// Contains all the constant here
+type contextKey string
+
+const (
+	EMPTY_STRING             = ""
+	INT_ZERO                 = 0
+	INT_ONE                  = 1
+	UNDERSCORE               = "_"
+	COLON                    = ":"
+	PORT                     = "8081"
+	REDIS_CLIENT_CONTEXT_KEY = "redisClient"
+	LOCAL_HOST               = "localhost"
+	REDIS_PORT               = "6379"
+	VALIDATION_ERROR         = "Validation"
+	VENDOR_ERROR             = "Vendor-Error"
+	API_KEY                  = "1bdf0f32f80a4aa8b35165559241903"
+)
